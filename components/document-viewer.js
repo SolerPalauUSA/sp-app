cd9e827ec538ef0f6fec575223351803391a1656
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-11-01 08:33:45 -0400

    box-shadow

--- components/document-viewer.js
+++ components/document-viewer.js
@@ -45,6 +45,7 @@ class LibraryComponent extends HTMLElement {
           font-size: 18px;
           color: #053658!important;
           transition: width 0.3s ease; /* Smooth transition for category buttons */
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
         }
         
         /* Set height when .categories.selected is active */
