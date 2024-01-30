78339b68651957e08a89e44208e5f2ca698d1a1c
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2024-01-30 09:39:47 -0500

    fix modal

--- components/document-viewer.js
+++ components/document-viewer.js
@@ -20,28 +20,28 @@ class LibraryComponent extends HTMLElement {
       .modal {
         display: none;
         position: fixed;
-        z-index: 1;
+        z-index: 9999;
         left: 0;
         top: 0;
         width: 100%;
-        height: 100%;
-        overflow: auto;
+        height: 100vh;
+        overflow: scroll;
         background-color: rgb(0,0,0);
         background-color: rgba(0,0,0,0.4);
       }
 
       .modal-content {
         background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
+        margin-left: 10px;
+        margin-right: 10px;
+        padding: 10px;
         border: 1px solid #888;
-        width: 80%;
       }
 
       .close {
         color: #aaa;
         float: right;
-        font-size: 28px;
+        font-size: 30px;
         font-weight: bold;
       }
 
