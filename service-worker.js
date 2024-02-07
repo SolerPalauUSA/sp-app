dc2365e2c7d18e9b7c1cb321fc6efe3cf950b08d
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2024-02-07 13:35:50 -0500

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Define the cache name and resources to cache
-const cacheName = 'Soler-Palau-App-Data-v1';
+const cacheName = 'Soler-Palau-App-Data-v2';
 const essentialCacheResources = [
   '/sp-app/',
   '/sp-app/index.html',
